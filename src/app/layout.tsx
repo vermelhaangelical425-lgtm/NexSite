0dd14ab0d6ff27965783572c20b92e9076c1f086
NexSite Admin <nexsite@example.com>
2026-09-22 22:04:10 -0300

    Add Google Site Verification metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,10 @@ export const metadata: Metadata = {
   description: "Plataforma de criação e venda de sites personalizados.",
   icons: {
     icon: "https://media.discordapp.net/attachments/1525567087785803856/1551807680224567377/AAAAAGSURBVAMAOtveHKOSHaQAAAAASUVORK5CYII.png?ex=6ab35106&is=6ab1ff86&hm=66712a96a5fe7414435b6a0df35680d668227fcb0f9f49a441eb1fc82b260de3&=&format=webp&quality=lossless"
-  }
+  },
+  verification: {
+    google: "vbN2nqjL8e_SZqz4gu3dJplW9b-LlWJ6fu1MuQ5h-3M",
+  },
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
